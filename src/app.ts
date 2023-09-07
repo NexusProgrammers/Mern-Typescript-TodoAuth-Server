616cb8e9f0417cd198b73f02e71b4e6413ed407d
Jawad Ullah <jawadullah156@gmail.com>
2023-09-07 11:07:57 -0700

    remove cors origin

--- src/app.ts
+++ src/app.ts
@@ -15,12 +15,7 @@ app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.use(morgan("dev"));
 
